0522ffa47789be44881e7713ca33ec82fce169c6
desoto13 <mrmisa_windel@yahoo.com>
2021-02-02 23:51:17 +0800

    Grid Chess

--- GridChess.js
+++ GridChess.js
@@ -1,6 +1,32 @@
-function moveFunc(obj) {
-    var Name = $(obj.className);
-    function RecFunc(Name,container) {
-        
+var moves = []
+var tile = []
+
+function printID(e) {
+    e = e || window.event;
+    e = e.target || e.srcElement;
+    let el = document.getElementById(e.id);
+    let piece = el.innerHTML;    
+
+    if (piece != "" && moves.length == 0) {
+        el.classList.toggle("blue");
+        moves.push(piece);
+        tile.push(el);
+    }
+    else if (piece == "" && moves.length != 0) {
+        el.innerHTML = moves[0];
+        tile[0].classList.toggle("blue");
+        tile[0].innerHTML = "";
+        moves.pop();
+        tile.pop();
+    }
+    else if (piece == "" && moves.length == 0) {
+        null;
+    }
+    else if (piece != "" && moves.length != 0) {
+        el.innerHTML = moves[0];
+        tile[0].classList.toggle("blue");
+        tile[0].innerHTML = "";
+        moves.pop();
+        tile.pop();
     }
 }
