691e4fc2187e6a89fe9db90081825285dff09410
desoto13 <mrmisa_windel@yahoo.com>
2021-02-13 19:23:00 +0800

    Product Landing Page

--- LandingPage.js
+++ LandingPage.js
@@ -37,6 +37,16 @@ function enableMute() {
   
 }
 
+// Burger
+// function Burger() {
+//   var x = document.getElementById("navbar");
+//   if (x.className === "navbar") {
+//     x.className += " responsive";
+//   } else {
+//     x.className = "navbar";
+//   }
+// }
+
 
 
 
