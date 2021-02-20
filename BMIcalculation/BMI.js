53c7f4b2ceae035e6726f53dc837f92a5526d5ce
desoto13 <mrmisa_windel@yahoo.com>
2021-02-20 10:15:18 +0800

    Avion School Projects

--- BMIcalculation/BMI.js
+++ BMIcalculation/BMI.js
@@ -11,8 +11,8 @@ function BMIcalculate(a,b,c,d) {
     var BMIVictor = c / (d*d);
     var compare = Boolean(BMIVictor>BMIJohn);
     var statement = "Is Victor's BMI Higher than John?";
-    console.log("John's BMI: ",BMIJohn);
-    console.log("Victor's BMI: ",BMIVictor);
+    console.log("John's BMI: ",BMIJohn.toFixed(2));
+    console.log("Victor's BMI: ",BMIVictor.toFixed(2));
     console.log(statement, compare);  
 }
 
