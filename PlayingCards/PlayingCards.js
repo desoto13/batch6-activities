412703f42c1953b7fb3677723260385f083fb1e9
desoto13 <mrmisa_windel@yahoo.com>
2021-03-05 21:27:12 +0800

    Playing Cards

--- PlayingCards/PlayingCards.js
+++ PlayingCards/PlayingCards.js
@@ -7,7 +7,6 @@ var deckObj = [];
 var deck = [];
 var message = "Input commands\n shuffle(deck) = shuffle  deck\n arrangeBySuits(deck) = sort deck by suits\n arrangeByValues(deck) = ascending sort deck\n arrangeByValuesDown(deck) = descending sort deck\n dealCard(deck) = draw one card\n dealFive(deck) = shuffle and draw 5 cards"
 
-
 for (i=1; i<14; i++) {
     order.push(i);
 }
@@ -25,18 +24,16 @@ console.log(deck)
 
 //Shuffle Cards
 function shuffle(deckOfCards) {
-    for (var i = 0; i < 1000; i++)
-	{
+    for (var i = 0; i < 1000; i++){
 		var location1 = Math.floor((Math.random() * deckOfCards.length));
 		var location2 = Math.floor((Math.random() * deckOfCards.length));
-		var tmp = deckOfCards[location1];
         var tmpObj = deckObj[location1];
-
-		deckOfCards[location1] = deckOfCards[location2];
-		deckOfCards[location2] = tmp;
         deckObj[location1] = deckObj[location2];
 		deckObj[location2] = tmpObj;
 	}
+    for (var x=0; x<deckObj.length; x++) {
+        deckOfCards[x] = deckObj[x].Card
+    }
     deck = deckOfCards;
     return deck;
 }
@@ -76,9 +73,7 @@ function dealCard(deckOfCards) {
     var topObj = deckObj.shift();
     var top = deckOfCards.shift();
     deck = deckOfCards;
-    console.log(`${topObj.Value} of ${topObj.Suit}`);
-    console.log(top)
-    console.log(deck);
+    console.log(`${topObj.Value} of ${topObj.Suit}`, top, deck);
 }
 
 //Draw 5 cards
@@ -97,30 +92,19 @@ function dealFive(deckOfCards) {
     var orderArr = [];
 
     //Input the cards into set and remove them from the deck
-    if (shuffleDeck.length>5){
-        for (i=0; i<5; i++) {
-            suitObj.add(fiveObj[i].Suit);
-            orderObj.add(fiveObj[i].Order);
-            orderArr.push(fiveObj[i].Order);
-            shuffleDeck.shift();
-            deckObj.shift();
-        }
-    }
-    else {
-        for (i=0; i<2; i++) {
-            suitObj.add(fiveObj[i].Suit);
-            orderObj.add(fiveObj[i].Order);
-            orderArr.push(fiveObj[i].Order);
-            shuffleDeck.shift();
-            deckObj.shift();
-        }
+    for (i=0; i<fiveCards.length; i++) {
+        suitObj.add(fiveObj[i].Suit);
+        orderObj.add(fiveObj[i].Order);
+        orderArr.push(fiveObj[i].Order);
+        shuffleDeck.shift();
+        deckObj.shift();
     }
 
     // Count the number of value cards in the hand
     var elcount = count(orderArr ,orderObj);
 
     //To determine the poker hand dealt
-    if (suitObj.size === 1 && !straight(orderObj).includes(false) && orderObj.size === 5) {
+    if (suitObj.size === 1 && straight(orderObj) && orderObj.size === 5) {
         pokerHand = "Straight Flush";
     }
     else if (elcount.includes(4)) {
@@ -132,7 +116,7 @@ function dealFive(deckOfCards) {
     else if (suitObj.size === 1) {
         pokerHand = "Flush";
     }
-    else if (!straight(orderObj).includes(false) && orderObj.size === 5) {
+    else if (straight(orderObj) && orderObj.size === 5) {
         pokerHand = "Straight"
     }
     else if (elcount.includes(1) && elcount.includes(3)) {
@@ -158,24 +142,21 @@ function dealFive(deckOfCards) {
 function straight(inputSet) {
     var prodarr = Array.from(inputSet);
     var sortArr = prodarr.sort((a,b) => (a > b) ? 1 : (a === b) ? 1 : -1);
-    var boolArr = [];
     for (i=0;i<sortArr.length-1;i++) {
         if (sortArr[i] === (sortArr[i+1]-1) || JSON.stringify(sortArr)===JSON.stringify([1,10,11,12,13])) {
-            boolArr.push(true)
+            continue
         }
         else {
-            boolArr.push(false)
-        }
-                    
+            return false
+        }            
     }
-    return boolArr;
+    return true;
 }
 
 //Count number of elements
 function count(arrInput, inputSet) {
     var output = [];
     var inputArr = Array.from(inputSet);
-
     for (i=0; i<inputArr.length ;i++) {
         var counter = 0;
         arrInput.forEach(element => {
@@ -185,8 +166,6 @@ function count(arrInput, inputSet) {
         });
         output.push(counter);
     }
-
-    return output;
-        
+    return output;   
 }
 
